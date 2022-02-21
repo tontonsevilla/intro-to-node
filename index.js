2eeba3d419b784163f73de0c6778f578fffc39f7
Winston Sevilla <winston_sevilla@outlook.com>
2022-02-21 13:20:32 +0800

    adding modules

---
+++ index.js
@@ -0,0 +1,5 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+console.log(`My Super Hero Name is: ${superheroes.random()}`);
+console.log(`My Super Villain Name is: ${supervillains.random()}`);
